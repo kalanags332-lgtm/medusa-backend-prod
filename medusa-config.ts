d91c11974777487b8545a1c21949f65101372250
kalanags331 <kalanags331@gmail.com>
2026-04-05 13:44:03 +0530

    fix: disable admin in production to prevent OOM crash

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,6 +3,9 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.NODE_ENV === "production",
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
